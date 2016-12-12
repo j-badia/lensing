baac2401de8ee6b9ca9f1ba70d52578480de843d
javobadia <javobadia@gmail.com>
2016-12-12 15:37:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ def _to_it(x):
     else:
         return (x,)
 
-def integrate(f, T, y0, t0=0, N=10000, params=None, check_photon_sphere=True):
+def integrate(f, T, y0, t0=0, N=00000, params=None, check_photon_sphere=True):
     """RK4 integrator
     Solves y' = f(t, y, *params) over interval T
     y may be a vector
     N: number of points to calculate; returns N+1 points
     """
-    h = T/N
     y0 = np.array(y0)
     if params is None:
         params = ()
@@ -34,6 +33,7 @@ def integrate(f, T, y0, t0=0, N=10000, params=None, check_photon_sphere=True):
         if check_photon_sphere:
             if y[n][0] < 1.5:
                 break
+        h = (T/N) * (1 - 0.95/y[n][0])
         k1 = g(t0 + n*h, y[n], *params)
         k2 = g(t0 + n*h + h/2, y[n] + (h/2)*k1, *params)
         k3 = g(t0 + n*h + h/2, y[n] + (h/2)*k2, *params)
@@ -53,11 +53,11 @@ def F(t, y, b):
     acceleration = 0.5*(-1+r)*(2*r**3 + b**2 * (5-7*r+2*r**2))/r**6
     return np.array((v, acceleration, b*(1-1/r)/r**2))
 
-n_rays = 100
+n_rays = 200
 
 pyplot.figure()
-pyplot.xlim(-2*r0, 2*r0)
-pyplot.ylim(-2*r0, 2*r0)
+pyplot.xlim(-r0/2, r0/2)
+pyplot.ylim(-r0/2, r0/2)
 pyplot.axes().set_aspect("equal")
 
 pyplot.plot([0],[0],"ok")
@@ -69,15 +69,32 @@ phi = np.linspace(0, 2*math.pi, 1000)
 pyplot.plot(r_h*np.cos(phi), r_h*np.sin(phi), 'k')
 pyplot.plot(r_ps*np.cos(phi), r_ps*np.sin(phi), 'k')
 
+ps_angle = math.atan(r_ps/r0) # half angle of the photon sphere
+#actually photons will fall in even if the starting angle is greater, because
+#they get deflected inwards (duh)
+
+b_ps = 9/2
+beta_ps = -math.acos(math.sqrt(1-1/r0)*b_ps/r0)
+alpha_ps = math.pi/2 - beta_ps
+
+def _cubic(x):
+    """Maps [0,1] to [0,1] like a cubic: flatter near x=1/2"""
+    return 0.5 * (2*(x-1/2))**3 + 1/2
+
 for i in range(n_rays):
-    beta = i * 2 * math.pi / n_rays
-    b = math.cos(beta) * r0 / (1-1/r0)
+    # alpha is measured from x-axis up, beta is measured from r=const to the right
+    alpha = 3*math.pi/4 + ((i/n_rays-1)**9 + 1) * math.pi/4
+    beta = math.pi/2 - alpha
+    b = math.cos(beta) * r0 / math.sqrt(1-1/r0)
     v0 = math.sin(beta) * (1-1/r0)
     (data, last_point) = integrate(F, 200, (r0,v0,0), N=n_points, params=(b,))
     (r, v, phi) = np.transpose(data[0:last_point+1])
     (x, y) = (r*np.cos(phi), r*np.sin(phi))
     pyplot.plot(x, y)
     pyplot.pause(0.0001)
-    print(i)
+    r_min = np.min(r)
+    print(i, alpha, last_point, b, r_min)
+    if r_min < 1.5:
+        break
 
 pyplot.show()
